01663fee8d5c6537c085a6fb6c9aba805f8fd476
enricoreich <enricor.devel@gmail.com>
2019-04-27 23:44:57 +0200

    create new blazorserverside project, copied things over and rename, some cleanup

--- samples/ServerSide/ChartJs.Blazor.Sample.Serverside/Startup.cs
+++ samples/ServerSide/ChartJs.Blazor.Sample.Serverside/Startup.cs
@@ -1,22 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ChartJs.Blazor.Sample.Serverside.Components;
 using Sotsera.Blazor.Toaster.Core.Models;
 
-namespace ChartJs.Blazor.Sample.Serverside
+namespace ChartJs.Blazor.Sample.ServerSide
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .AddNewtonsoftJson();
-
-            services.AddRazorComponents();
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
 
             services.AddToaster(config =>
             {
@@ -26,7 +25,6 @@ namespace ChartJs.Blazor.Sample.Serverside
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -35,21 +33,22 @@ namespace ChartJs.Blazor.Sample.Serverside
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             // Add the JS interop file to the webRooPath - make sure to reference it from index.cshtml
             ChartJsBlazor.AddStaticResourcesToWebRootPath(env.WebRootPath);
 
+            app.UseRouting();
 
-            app.UseRouting(routes =>
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRazorPages();
-                routes.MapComponentHub<App>("app");
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
